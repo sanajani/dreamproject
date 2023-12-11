a0747e429b7f4f9e53c1a0833a186280f2965625
Sanaullah <webdevfarsi@gmail.com>
2023-12-11 22:44:45 +0430

    userModel is created

---
+++ server/models/userModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Name field is required"]
+    },
+    email:{
+        type:String,
+        required:[true, "Email field is required"],
+        unique: [true, "Email field should be unique"]
+    },
+    password:{
+        type:String,
+        required:[true, "Password field is required"],
+        unique: [true, "Password field should be unique"]
+    },
+})
+
+const userModel = mongoose.model("users",userSchema)
+export default userModel
