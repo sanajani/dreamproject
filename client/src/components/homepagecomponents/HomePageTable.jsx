698e48255ec6fb7560dab51ec3080e7355b5fa3a
Sanaullah <webdevfarsi@gmail.com>
2023-12-13 11:09:44 +0430

    changed

--- client/src/components/homepagecomponents/HomePageTable.jsx
+++ client/src/components/homepagecomponents/HomePageTable.jsx
@@ -1,5 +1,3 @@
-// import { Link } from 'react-router-dom'
-// import { useState } from 'react'
 import { data } from '../../utils/mock_data'
 import {flexRender,getCoreRowModel,useReactTable} from '@tanstack/react-table'
 import { columns } from '../../utils/tanstacktable/columnHelper'
